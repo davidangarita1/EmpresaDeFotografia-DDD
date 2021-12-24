3d94d918f0fc6b253a4e8094b899bd8c40c13d40
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-23 21:02:05 -0500

    Agregado retocador terminado

--- src/main/java/co/com/sofka/domain/Retocador/RetocadorChange.java
+++ src/main/java/co/com/sofka/domain/Retocador/RetocadorChange.java
@@ -1,7 +1,6 @@
 package co.com.sofka.domain.Retocador;
 
-import co.com.sofka.domain.Retocador.Event.CaracteristicaDeFuncionActualizada;
-import co.com.sofka.domain.Retocador.Event.RetocadorCreado;
+import co.com.sofka.domain.Retocador.Event.*;
 import co.com.sofka.domain.generic.EventChange;
 
 import java.util.HashSet;
@@ -17,7 +16,80 @@ public class RetocadorChange extends EventChange {
         });
 
         apply((CaracteristicaDeFuncionActualizada event) -> {
-
+            var funcion = retocador.getFuncionPorId(event.getIdFuncion())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra la caracteristica de la función"));
+            funcion.actualizarCaracteristica(event.getCaracteristica());
         });
+
+        apply((DescripcionDeFuncionActualizada event) ->{
+            var funcion = retocador.getFuncionPorId(event.getIdFuncion())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra la descripción de la función"));
+            funcion.actualizarDescripcion(event.getDescripcion());
+        });
+
+        apply((DescripcionDeTareaActualizada event) -> {
+            var tarea = retocador.getTareaPorId(event.getIdTarea())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra la descripción de la tarea"));
+            tarea.actualizarDescripcion(event.getDescripcion());
+        });
+
+        apply((FechaFinalDeTareaActualizada event) -> {
+            var tarea = retocador.getTareaPorId(event.getIdTarea())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra la fecha final de la tarea"));
+            tarea.actualizarFechaFinal(event.getFechaFinal());
+        });
+
+        apply((FechaInicialDeTareaActualiada event) -> {
+            var tarea = retocador.getTareaPorId(event.getIdTarea())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra la fecha inicial de la tarea"));
+            tarea.actualizarFechaInicial(event.getFechaInicial());
+        });
+
+        apply((FormatoDeFotoActualizado event) -> {
+            var foto = retocador.getFotoPorId(event.getIdFoto())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra el formato de la foto"));
+            foto.actualizarFormato(event.getFormato());
+        });
+
+        apply((FotoAgregada event) -> {
+            retocador.fotos.add(new Foto(
+                    event.getIdFoto(),
+                    event.getFormato(),
+                    event.getPerfilDeColor()
+            ));
+        });
+
+        apply((FuncionAgregada event) -> {
+            var numFunciones = retocador.funciones().size();
+            if (numFunciones == 10) {
+                throw  new IllegalArgumentException("No puedes agregar mas funciones, solo puedes tener hasta 10 de ellas");
+            }
+            retocador.funciones.add(new Funcion(
+                    event.getIdFuncion(),
+                    event.getCaracteristica(),
+                    event.getDescripcion()
+            ));
+        });
+
+        apply((TareaAgregada event) -> {
+            retocador.tareas.add(new Tarea(
+                    event.getIdTarea(),
+                    event.getFechaInicial(),
+                    event.getFechaFinal(),
+                    event.getDescripcion()
+            ));
+        });
+
+        apply((NombreActualizado event) -> {
+            retocador.nombre = event.getNombre();
+        });
+
+        apply((PerfilDeColorDeFotoActualizado event) -> {
+            var foto = retocador.getFotoPorId(event.getIdFoto())
+                    .orElseThrow(() -> new IllegalArgumentException("No se encuentra el perfil de color de la foto"));
+            foto.actualizarPerfilDeColor(event.getPerfilDeColor());
+        });
+
+
     }
 }
